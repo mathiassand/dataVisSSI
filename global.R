53507ac54981b2619dece9761c8db8930611e70d
msk4 <mskr16@student.aau.dk>
2020-10-06 18:46:35 +0200

    it works :)

--- global.R
+++ global.R
@@ -9,29 +9,60 @@ library(magrittr)
 library(here)
 library(readr)
 library(tidyr)
+library(tidyverse)
+library(stringr)
 
 dsize <- read_delim(here("Municipality_test_pos.csv"), ";", escape_double = FALSE, trim_ws = TRUE)
+dt <- read_delim(here("Municipality_tested_persons_time_series.csv"), ";", escape_double = FALSE, trim_ws = TRUE)
 dk <- st_read("shapefiles/gadm36_DNK_2.shp")
 
 
-ProcessData<-function(dc){
+
+
+ProcessData <- function(dc) {
+  deg2rad <- function(x) {
+    radian <- x * pi / 180
+    return(radian)
+  }
   
-  {{dc}} %<>%
+  dc %<>%
     pivot_longer(cols = !date_sample, names_to = "kommune", values_to = "casesDiagnosed") %>% arrange(kommune, date_sample)
   
-  sum(is.na(dc$casesDiagnosed))
+  dt %<>%
+    pivot_longer(cols = !PrDate_adjusted, names_to = "kommune", values_to = "testsConducted") %>%
+    arrange(kommune, PrDate_adjusted) %>%
+    rename(date_sample = PrDate_adjusted)
   
+  # check there are no NA in the set
+  sum(is.na(dc$casesDiagnosed))
+  sum(is.na(dt$testsConducted))
+  sum(is.na(dt$testsConducted))
+  sum(is.na(dsize$Befolkningstal))
+  
+  # make data compatible rename columns for merging and
   dsize <- dsize %>%
     select(contains("Kom"), Befolkningstal) %>%
     rename("kID" = `Kommune_(id)`, "kommune" = `Kommune_(navn)`, population = Befolkningstal) %>%
     mutate(population = population * 1000) %>%
     select(-kID)
   
+  # check Kommune name can be used as key
   dc[!(dc$kommune %in% dsize$kommune), ]$kommune
-  dc$kommune <- str_replace(dc$kommune, "Copenhagen", "København")
-  dc <- merge(dc, dsize)
+  dsize[!(dsize$kommune %in% dt$kommune), ]$kommune
+  dt[!(dt$kommune %in% dsize$kommune), ]$kommune
   
-  {{dc}} %<>%
+  # ooops!
+  # for some reason one data frame uses Københaven the other Copenhagen),
+  dc$kommune <- str_replace(dc$kommune, "Copenhagen", "København")
+  dt$kommune <- str_replace(dt$kommune, "Copenhagen", "København")
+  # check again keys are clean OK re-run rows 35-37
+  
+  # merge data together
+  dc <- merge(dc, dsize)
+  dt <- merge(dt, dsize)
+  
+  # create new variables (e.g. rolling aggregates)
+  dc %<>%
     group_by(kommune) %>%
     mutate(
       casesDPer100k = casesDiagnosed / (population / 1000),
@@ -42,67 +73,87 @@ ProcessData<-function(dc){
       dcr7dPer100kCh7 = lag(dcr7dPer100k, 7) - dcr7dPer100k
     )
   
+  dt %<>%
+    group_by(kommune) %>%
+    mutate(
+      testsPer100k = testsConducted / (population / 1000),
+      tr7d = roll_sum(testsConducted, width = 7, min_obs = 1),
+      tr7dP100k = tr7d / (population / 1000),
+      tr7dP100kCh1 = lag(tr7dP100k, 1) - tr7dP100k,
+      tr7dP100kCh3 = lag(tr7dP100k, 3) - tr7dP100k,
+      tr7dP100kCh7 = lag(tr7dP100k, 7) - tr7dP100k
+    )
+  
+  # merge test with case data
+  # samsø doesn't exist in the dc dataset, which is why we cannot plot anything from it on the interactive map,
+  # as we are only plotting changes in the confirmed cases (dc), and not tests conducted (dt)
+  df <- merge(dc, dt)
+  
+  df$PosTestRate7d <- df$dcr7dPer100k / df$tr7dP100k
+  
+  # check there were no diagnosis without tests
+  sum(df$casesDiagnosed > df$testsConducted)
   dk$NAME_2 <- str_replace(dk$NAME_2, "Århus", "Aarhus")
   dk$NAME_2 <- str_replace(dk$NAME_2, "Høje Taastrup", "Høje-Taastrup")
   dk$NAME_2 <- str_replace(dk$NAME_2, "Vesthimmerland", "Vesthimmerlands")
-  
-  # joining the population to the shapefile 
+
+  # joining the population to the shapefile
   dk_pop <-
     dsize %>%
     left_join(dk, by = c("kommune" = "NAME_2"))
-  
+
   # transforming the df into a shapefile again, that makes us use the geometry points
   df_dk <- st_as_sf(dk_pop, sf_column_name = "geometry")
-  
+
   # getting the centroids to grab the coordinates from the dataset
   dk_cent <- st_centroid(df_dk)
   dk_coords <- st_coordinates(dk_cent)
-  
+
   # converting the matrix into a df again
   dk_coords_next <- as.data.frame(dk_coords)
-  
+
   # adding the coordinates to the kommunes and their population
   dk_merge_coords <-
     dsize %>%
     cbind(dk_coords_next)
-  
+
   # merging the coords/kommunes with the covid data
   dk_merge_coords_test <-
     df %>%
     merge(dk_merge_coords)
-  
+
   # merging the covid data into the shapefile to plot it
   df_dk_covid <-
     df %>%
     filter(date_sample == "2020-09-17") %>%
     merge(df_dk)
-  
+
   # to plot the data it needs to be a shapefile (sf) again - creating shapefile
   # and specifying where it should take sf data from
-  df_dk_covid <- 
+  df_dk_covid <-
     st_as_sf(df_dk_covid, sf_column_name = "geometry")
-  
-  #making another sf to plot copenhagen as a seperate map
-  df_cph_covid<-
-    df_dk_covid%>%
-    filter(NAME_1=="Hovedstaden")
-  
+
+  # making another sf to plot copenhagen as a seperate map
+  df_cph_covid <-
+    df_dk_covid %>%
+    filter(NAME_1 == "Hovedstaden")
+
   # filtering for a single date to not cause overplotting
   a_one_date <- dk_merge_coords_test %>%
     filter(date_sample == "2020-09-17")
-  
-  
+
+
   ########### MAP###########
-  
+
   # getting the absolute values - converting the negative values into 0
   # to later normalize
-  
+
   scaleFactor <- 20
-  
+
   a_one_date$dcr7dPer100kCh1Col <- plyr::mapvalues(sign(a_one_date$dcr7dPer100kCh1), from = c(1, 0, -1), to = c("#FF0000", "#00FFFF", "#00FF00"))
   a_one_date$dcr7dPer100kCh3Col <- plyr::mapvalues(sign(a_one_date$dcr7dPer100kCh3), from = c(1, 0, -1), to = c("#FF0000", "#00FFFF", "#00FF00"))
   a_one_date$dcr7dPer100kCh7Col <- plyr::mapvalues(sign(a_one_date$dcr7dPer100kCh7), from = c(1, 0, -1), to = c("#FF0000", "#00FFFF", "#00FF00"))
-  
+
   a_one_date %<>%
     mutate(
       y_dcr7dPer100kCh1 = (plogis(abs(dcr7dPer100kCh1) * 10) - 0.5) * 188,
@@ -122,6 +173,3 @@ ProcessData<-function(dc){
       custlat_Ch7 = Y + sign(dcr7dPer100kCh7) * (custlat_dcr7dPer100kCh7)
     )
 }
-
-
-
--- server.R
+++ server.R
@@ -5,10 +5,14 @@
 # Find out more about building applications with Shiny here:
 #
 #    http://shiny.rstudio.com/
-#
+
+
 
 # Define server logic required to draw a histogram
+
+
 server <- (function(input, output) {
+
   # input$file1 will be NULL initially. After the user selects and uploads a
   # file, it will be a data frame with 'name', 'size', 'type', and 'datapath'
   # columns. The 'datapath' column will contain the local filenames where the
@@ -23,14 +27,16 @@ server <- (function(input, output) {
     }
 
     dc <- read.csv(inFile$datapath, header = input$header, sep = input$sep, quote = input$quote)
+    
+    ProcessData(dc)
   })
 
-  finalInput <- reactive({
-    if (!input$ProcessData) {
-      return(dataInput())
-    }
-    ProcessData(dataInput())
-  })
+  # finalInput <- reactive({
+  #   if (!input$ProcessData) {
+  #     return(dataInput())
+  #   }
+  #   
+  # })
 
   output$myMap <- renderLeaflet({
     # Use leaflet() here, and only include aspects of the map that
@@ -41,18 +47,72 @@ server <- (function(input, output) {
       setView(lng = 9.501785, lat = 56.26392, zoom = 7) %>%
       addProviderTiles(providers$CartoDB.Positron)
   })
-  
-  
+
+
+    observe({
+      leafletProxy("myMap", data = dataInput()) %>%
+        addPolygons(
+          data = df_dk_covid, color = "#444444", weight = 1, smoothFactor = 0.5,
+          opacity = 1.0, fillOpacity = 0.5,
+          fillColor = ~colorQuantile("YlOrRd", dcr7dPer100k)(dcr7dPer100k)
+        )
+    })
+
   observe({
-    leafletProxy("myMap", data = finalInput()) %>%
-      addPolygons(
-        data = dk, color = "#444444", weight = 1, smoothFactor = 0.5,
-        opacity = 1.0, fillOpacity = 0.5,
-        fillColor = ~ colorQuantile("YlOrRd", dcr7dPer100k)(dcr7dPer100k)
-      )
+    leafletProxy("myMap", data = dataInput()) %>%
+      addCircleMarkers(lng = ~X, lat = ~Y, radius = 6, data = a_one_date, weight = 1, stroke = F, fillOpacity = .5, color = "#808080")
   })
 
-  
+  observe({
+    for (i in 1:nrow(a_one_date)) {
+    leafletProxy("myMap", data = dataInput()) %>%
+        addPolylines(
+          data = a_one_date[i, ],
+          lng = ~ c(X, custlng_Ch1),
+          lat = ~ c(Y, custlat_Ch1),
+          color = ~dcr7dPer100kCh1Col,
+          weight = 4,
+          group = "Change since the day before",
+        )
+    }
+  })
+
+  observe({
+    for (i in 1:nrow(a_one_date)) {
+        leafletProxy("myMap", data = dataInput()) %>%
+        addPolylines(
+          data = a_one_date[i, ],
+          lng = ~ c(X, custlng_Ch3),
+          lat = ~ c(Y, custlat_Ch3),
+          color = ~dcr7dPer100kCh7Col,
+          weight = 4,
+          group = "Change the last 3 days",
+        )
+    }
+  })
+
+  observe({
+    for (i in 1:nrow(a_one_date)) {
+        leafletProxy("myMap", data = dataInput()) %>%
+        addPolylines(
+          data = a_one_date[i, ],
+          lng = ~ c(X, custlng_Ch7),
+          lat = ~ c(Y, custlat_Ch7),
+          color = ~dcr7dPer100kCh7Col,
+          weight = 4,
+          group = "Change the last 7 days",
+        )
+    }
+  })
+
+  observe({
+    myMap<- leafletProxy("myMap", data = dataInput()) %>%
+      addLayersControl(
+        overlayGroups = c("Change since the day before", "Change the last 3 days", "Change the last 7 days"),
+        options = layersControlOptions(collapsed = FALSE),
+      )
+  })
 })
+
 
 # https://github.com/stefanocudini/leaflet-panel-layers
