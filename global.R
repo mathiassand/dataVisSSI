537ee86cd043555ceed5bc9ca2184e8b65fa79fb
mathiassand <mskr16@student.aau.dk>
2020-10-08 14:29:17 +0200

    trying to swap around dataprocessing
    need to create functions for processing shapefile and combining shapefile + df

--- global.R
+++ global.R
@@ -15,8 +15,26 @@ library(stringr)
 
 dsize <- read_delim(here("Municipality_test_pos.csv"), ";", escape_double = FALSE, trim_ws = TRUE)
 dt <- read_delim(here("Municipality_tested_persons_time_series.csv"), ";", escape_double = FALSE, trim_ws = TRUE)
+
+dt %<>%
+  pivot_longer(cols = !PrDate_adjusted, names_to = "kommune", values_to = "testsConducted") %>%
+  arrange(kommune, PrDate_adjusted) %>%
+  rename(date_sample = PrDate_adjusted)
+
 dk <- st_read("shapefiles/gadm36_DNK_2.shp")
-browser()
+dk$NAME_2 <- str_replace(dk$NAME_2, "Århus", "Aarhus")
+dk$NAME_2 <- str_replace(dk$NAME_2, "Høje Taastrup", "Høje-Taastrup")
+dk$NAME_2 <- str_replace(dk$NAME_2, "Vesthimmerland", "Vesthimmerlands")
+
+sum(is.na(dsize$Befolkningstal))
+
+dsize <- dsize %>%
+  select(contains("Kom"), Befolkningstal) %>%
+  rename("kID" = `Kommune_(id)`, "kommune" = `Kommune_(navn)`, population = Befolkningstal) %>%
+  mutate(population = population * 1000) %>%
+  select(-kID)
+
+dsize[!(dsize$kommune %in% dt$kommune), ]$kommune
 
 ProcessData <- function(dc) {
   
@@ -28,27 +46,12 @@ ProcessData <- function(dc) {
   dc %<>%
     pivot_longer(cols = !date_sample, names_to = "kommune", values_to = "casesDiagnosed") %>% arrange(kommune, date_sample)
   
-  dt %<>%
-    pivot_longer(cols = !PrDate_adjusted, names_to = "kommune", values_to = "testsConducted") %>%
-    arrange(kommune, PrDate_adjusted) %>%
-    rename(date_sample = PrDate_adjusted)
-  
   # check there are no NA in the set
   sum(is.na(dc$casesDiagnosed))
   sum(is.na(dt$testsConducted))
-  sum(is.na(dt$testsConducted))
-  sum(is.na(dsize$Befolkningstal))
-  
-  # make data compatible rename columns for merging and
-  dsize <- dsize %>%
-    select(contains("Kom"), Befolkningstal) %>%
-    rename("kID" = `Kommune_(id)`, "kommune" = `Kommune_(navn)`, population = Befolkningstal) %>%
-    mutate(population = population * 1000) %>%
-    select(-kID)
-  
+
   # check Kommune name can be used as key
   dc[!(dc$kommune %in% dsize$kommune), ]$kommune
-  dsize[!(dsize$kommune %in% dt$kommune), ]$kommune
   dt[!(dt$kommune %in% dsize$kommune), ]$kommune
   
   # ooops!
@@ -95,80 +98,8 @@ ProcessData <- function(dc) {
   sum(df$casesDiagnosed > df$testsConducted)
   
   
-  dk$NAME_2 <- str_replace(dk$NAME_2, "Århus", "Aarhus")
-  dk$NAME_2 <- str_replace(dk$NAME_2, "Høje Taastrup", "Høje-Taastrup")
-  dk$NAME_2 <- str_replace(dk$NAME_2, "Vesthimmerland", "Vesthimmerlands")
-
-  # joining the population to the shapefile
-  dk_pop <-
-    dsize %>%
-    left_join(dk, by = c("kommune" = "NAME_2"))
-
-  # transforming the df into a shapefile again, that makes us use the geometry points
-  df_dk <- st_as_sf(dk_pop, sf_column_name = "geometry")
-
-  # getting the centroids to grab the coordinates from the dataset
-  dk_cent <- st_centroid(df_dk)
-  dk_coords <- st_coordinates(dk_cent)
-
-  # converting the matrix into a df again
-  dk_coords_next <- as.data.frame(dk_coords)
-
-  # adding the coordinates to the kommunes and their population
-  dk_merge_coords <-
-    dsize %>%
-    cbind(dk_coords_next)
-
-  # merging the coords/kommunes with the covid data
-  dk_merge_coords_test <-
-    dc %>%
-    merge(dk_merge_coords)
-
-  # merging the covid data into the shapefile to plot it
-  df_dk_covid <-
-    dc %>%
-    filter(date_sample == "2020-09-17") %>%
-    merge(df_dk)
-
-  # to plot the data it needs to be a shapefile (sf) again - creating shapefile
-  # and specifying where it should take sf data from
-   df_dk_covid <-
-    st_as_sf(df_dk_covid, sf_column_name = "geometry")
-
-  # filtering for a single date to not cause overplotting
-  a_one_date <- dk_merge_coords_test %>%
-    filter(date_sample == "2020-09-17")
 
 
-  ########### MAP ###########
-
-  # getting the absolute values - converting the negative values into 0
-  # to later normalize
-
-  scaleFactor <- 20
-
-  a_one_date$dcr7dPer100kCh1Col <- plyr::mapvalues(sign(a_one_date$dcr7dPer100kCh1), from = c(1, 0, -1), to = c("#FF0000", "#00FFFF", "#00FF00"))
-  a_one_date$dcr7dPer100kCh3Col <- plyr::mapvalues(sign(a_one_date$dcr7dPer100kCh3), from = c(1, 0, -1), to = c("#FF0000", "#00FFFF", "#00FF00"))
-  a_one_date$dcr7dPer100kCh7Col <- plyr::mapvalues(sign(a_one_date$dcr7dPer100kCh7), from = c(1, 0, -1), to = c("#FF0000", "#00FFFF", "#00FF00"))
-
-  a_one_date %<>%
-    mutate(
-      y_dcr7dPer100kCh1 = (plogis(abs(dcr7dPer100kCh1) * 10) - 0.5) * 188,
-      y_dcr7dPer100kCh3 = (plogis(abs(dcr7dPer100kCh3) * 10) - 0.5) * 188,
-      y_dcr7dPer100kCh7 = (plogis(abs(dcr7dPer100kCh7) * 10) - 0.5) * 188,
-      custlng_dcr7dPer100kCh1 = cos(deg2rad(y_dcr7dPer100kCh1)) / scaleFactor,
-      custlng_dcr7dPer100kCh3 = cos(deg2rad(y_dcr7dPer100kCh3)) / scaleFactor,
-      custlng_dcr7dPer100kCh7 = cos(deg2rad(y_dcr7dPer100kCh7)) / scaleFactor,
-      custlat_dcr7dPer100kCh1 = sin(deg2rad(y_dcr7dPer100kCh1)) / scaleFactor,
-      custlat_dcr7dPer100kCh3 = sin(deg2rad(y_dcr7dPer100kCh3)) / scaleFactor,
-      custlat_dcr7dPer100kCh7 = sin(deg2rad(y_dcr7dPer100kCh7)) / scaleFactor,
-      custlng_Ch1 = X + (custlng_dcr7dPer100kCh1),
-      custlng_Ch3 = X + (custlng_dcr7dPer100kCh3),
-      custlng_Ch7 = X + (custlng_dcr7dPer100kCh7),
-      custlat_Ch1 = Y + sign(dcr7dPer100kCh1) * (custlat_dcr7dPer100kCh1),
-      custlat_Ch3 = Y + sign(dcr7dPer100kCh3) * (custlat_dcr7dPer100kCh3),
-      custlat_Ch7 = Y + sign(dcr7dPer100kCh7) * (custlat_dcr7dPer100kCh7)
-    )
 }
 
 
--- server.R
+++ server.R
@@ -38,20 +38,84 @@ server <- (function(input, output) {
     # entire map is being torn down and recreated).
     dk_data<-finalInput()
     
+    
+    
+    dk_pop <-
+      dsize %>%
+      left_join(dk, by = c("kommune" = "NAME_2"))
+    
+    sf_dk <- st_as_sf(dk_pop, sf_column_name = "geometry")
+    # getting the centroids to grab the coordinates from the dataset
+    dk_cent <- st_centroid(sf_dk)
+    dk_coords <- st_coordinates(dk_cent)
+    
+    # converting the matrix into a df again
+    dk_coords_next <- as.data.frame(dk_coords)
+    
+    # adding the coordinates to the kommunes and their population
+    dk_merge_coords <-
+      dsize %>%
+      cbind(dk_coords_next)
+    
+    # merging the coords/kommunes with the covid data
+    dk_merge_coords_test <-
+      dk_data %>%
+      merge(dk_merge_coords)
+    
+    # merging the covid data into the shapefile to plot it
+    df_dk_covid <-
+      dk_data %>%
+      dplyr::filter(date_sample == "2020-09-16") %>%
+      merge(sf_dk)
+    
+    # to plot the data it needs to be a shapefile (sf) again - creating shapefile
+    # and specifying where it should take sf data from
+    sf_dk_covid <-
+      st_as_sf(sf_dk_covid, sf_column_name = "geometry")
+    
+    # filtering for a single date to not cause overplotting
+    a_one_date <- dk_merge_coords_test %>%
+      dplyr::filter(date_sample == "2020-09-16")
+    
+    scaleFactor <- 20
+    
+    a_one_date$dcr7dPer100kCh1Col <- plyr::mapvalues(sign(a_one_date$dcr7dPer100kCh1), from = c(1, 0, -1), to = c("#FF0000", "#00FFFF", "#00FF00"))
+    a_one_date$dcr7dPer100kCh3Col <- plyr::mapvalues(sign(a_one_date$dcr7dPer100kCh3), from = c(1, 0, -1), to = c("#FF0000", "#00FFFF", "#00FF00"))
+    a_one_date$dcr7dPer100kCh7Col <- plyr::mapvalues(sign(a_one_date$dcr7dPer100kCh7), from = c(1, 0, -1), to = c("#FF0000", "#00FFFF", "#00FF00"))
+    
+    a_one_date %<>%
+      mutate(
+        y_dcr7dPer100kCh1 = (plogis(abs(dcr7dPer100kCh1) * 10) - 0.5) * 188,
+        y_dcr7dPer100kCh3 = (plogis(abs(dcr7dPer100kCh3) * 10) - 0.5) * 188,
+        y_dcr7dPer100kCh7 = (plogis(abs(dcr7dPer100kCh7) * 10) - 0.5) * 188,
+        custlng_dcr7dPer100kCh1 = cos(deg2rad(y_dcr7dPer100kCh1)) / scaleFactor,
+        custlng_dcr7dPer100kCh3 = cos(deg2rad(y_dcr7dPer100kCh3)) / scaleFactor,
+        custlng_dcr7dPer100kCh7 = cos(deg2rad(y_dcr7dPer100kCh7)) / scaleFactor,
+        custlat_dcr7dPer100kCh1 = sin(deg2rad(y_dcr7dPer100kCh1)) / scaleFactor,
+        custlat_dcr7dPer100kCh3 = sin(deg2rad(y_dcr7dPer100kCh3)) / scaleFactor,
+        custlat_dcr7dPer100kCh7 = sin(deg2rad(y_dcr7dPer100kCh7)) / scaleFactor,
+        custlng_Ch1 = X + (custlng_dcr7dPer100kCh1),
+        custlng_Ch3 = X + (custlng_dcr7dPer100kCh3),
+        custlng_Ch7 = X + (custlng_dcr7dPer100kCh7),
+        custlat_Ch1 = Y + sign(dcr7dPer100kCh1) * (custlat_dcr7dPer100kCh1),
+        custlat_Ch3 = Y + sign(dcr7dPer100kCh3) * (custlat_dcr7dPer100kCh3),
+        custlat_Ch7 = Y + sign(dcr7dPer100kCh7) * (custlat_dcr7dPer100kCh7)
+      )
+    
     map<-leaflet(data = dk_data) %>%
       addTiles() %>%
       setView(lng = 9.501785, lat = 56.26392, zoom = 7) %>%
       addProviderTiles(providers$CartoDB.Positron) %>%
       addPolygons(
-        data = dk, color = "#444444", weight = 1, smoothFactor = 0.5,
+        data = df_dk_covid, color = "#444444", weight = 1, smoothFactor = 0.5,
         opacity = 1.0, fillOpacity = 0.5,
-        fillColor = ~colorQuantile("YlOrRd", dk_data$dcr7dPer100k)(dk_data$dcr7dPer100k)) %>%
-          addCircleMarkers(lng = dk_data$X, lat = dk_data$Y, radius = 6, data = dk_data,
+        fillColor = ~colorQuantile("YlOrRd", dcr7dPer100k)(dcr7dPer100k)) %>%
+          addCircleMarkers(lng = ~X, lat = ~Y, radius = 6, data = a_one_date,
                            weight = 1, stroke = F, fillOpacity = .5, color = "#808080")
-          for (i in 1:nrow(dk_data)) {
+          for (i in 1:nrow(a_one_date)) {
             map <- map %>%
                     addPolylines(
-                      data = dk_data[i, ],
+                      data = a_one_date[i, ],
                       lng = ~ c(X, custlng_Ch1),
                       lat = ~ c(Y, custlat_Ch1),
                       color = ~dcr7dPer100kCh1Col,
@@ -59,10 +123,10 @@ server <- (function(input, output) {
                       group = "Change since the day before",
                     )
           }
-          for (i in 1:nrow(dk_data)) {
+          for (i in 1:nrow(a_one_date)) {
             map <- map %>%
               addPolylines(
-                data = dk_data[i, ],
+                data = a_one_date[i, ],
                 lng = ~ c(X, custlng_Ch3),
                 lat = ~ c(Y, custlat_Ch3),
                 color = ~dcr7dPer100kCh3Col,
@@ -70,10 +134,10 @@ server <- (function(input, output) {
                 group = "Change the last 3 days",
               )
           }
-          for (i in 1:nrow(dk_data)) {
+          for (i in 1:nrow(a_one_date)) {
             map <- map %>%
               addPolylines(
-                data = dk_data[i, ],
+                data = a_one_date[i, ],
                 lng = ~ c(X, custlng_Ch7),
                 lat = ~ c(Y, custlat_Ch7),
                 color = ~dcr7dPer100kCh7Col,
